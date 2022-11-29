171aeef2c82f4867203fce2f6da2d47663cc5243
Bruno Domingues <bruno.domingues@lighthouseit.com.br>
2022-11-28 21:20:27 -0300

    feat: boilerplate started

---
+++ src/components/test/index.tsx
@@ -0,0 +1,3 @@
+export function Teste() {
+  return <div className="text-lg text-red-500 bg-slate-900">TESTE</div>;
+}
---
+++ src/components/test/test.stories.tsx
@@ -0,0 +1,10 @@
+import { Teste } from ".";
+
+const Template = () => <Teste />;
+
+export const Default = Template.bind({});
+
+export default {
+  title: "Teste",
+  component: Teste,
+};
